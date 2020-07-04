d3dc44ec63e2490ac367e6e191e15aa9ed96b69e
mantou132 <709922234@qq.com>
2020-07-04 14:49:11 +0800

    Add pwa support

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,9 @@ import { render } from 'react-dom';
 import App from 'src/App';
 
 render(<App />, document.body.appendChild(document.createElement('div')));
+
+if (navigator.serviceWorker) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const { GenerateSW } = require('workbox-webpack-plugin');
+
+/**
+ * @type {import('webpack-pwa-manifest').ManifestOptions}
+ */
+const webManifestConfig = {
+  filename: 'manifest.webmanifest',
+  fingerprints: false,
+  name: 'My Progressive Web App',
+  short_name: 'MyPWA',
+  description: 'My awesome Progressive Web App!',
+  theme_color: '#ffffff',
+  background_color: '#ffffff',
+  icons: [
+    {
+      src: 'src/logo.svg',
+      sizes: [96, 128, 192, 256, 384, 512],
+      purpose: 'any maskable',
+    },
+  ],
+};
 
 /**
  * @type {import('webpack/declarations/WebpackOptions').WebpackOptions}
@@ -24,7 +45,7 @@ module.exports = {
         use: ['@svgr/webpack', 'url-loader'],
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(bmp|gif|jpe?g|png|webp)$/,
         use: ['file-loader'],
       },
     ],
@@ -41,7 +62,14 @@ module.exports = {
   plugins: [
     new ManifestPlugin(),
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: webManifestConfig.name,
+      favicon: webManifestConfig.icons[0].src,
+      meta: {
+        description: webManifestConfig.description,
+        viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      },
+    }),
     new PreloadWebpackPlugin({
       rel: 'preload',
       include: 'initial',
@@ -51,9 +79,8 @@ module.exports = {
       rel: 'prefetch',
       include: 'asyncChunks',
     }),
-    new CopyWebpackPlugin({ patterns: [{ from: './public', to: './' }] }),
-    // manifest
-    // service worker
+    new WebpackPwaManifest(webManifestConfig),
+    new GenerateSW(),
   ],
   devServer: {
     contentBase: './build',
